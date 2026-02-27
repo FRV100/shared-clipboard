e7c488192a22e6728e28039135b3619971145ffe
FRV100 <francois.voyat@gmail.com>
2026-02-27 09:59:48 +0100

    🎉 Begin frontend web in SolidJS with Tailwind CSS

---
+++ web/vite.config.js
@@ -0,0 +1,21 @@
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import solidPlugin from 'vite-plugin-solid';
+import devtools from 'solid-devtools/vite';
+
+export default defineConfig({
+  plugins: [devtools(), solidPlugin(), tailwindcss()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': 'http://localhost:3000',
+      '/ws': {
+        target: 'ws://localhost:3000',
+        ws: true,
+      },
+    },
+  },
+  build: {
+    target: 'esnext',
+  },
+});
