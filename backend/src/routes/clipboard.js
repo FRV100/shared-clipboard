21e7da6ec222c4ada3d3079b2e63d0d61284ee62
FRV100 <francois.voyat@gmail.com>
2026-02-27 08:57:22 +0100

    🥅 Add error handling to clipboard API

--- backend/src/routes/clipboard.js
+++ backend/src/routes/clipboard.js
@@ -4,23 +4,34 @@ import db from '../db.js';
 const router = express.Router();
 
 router.get('/clipboard', (req, res) => {
-  const row = db.query(`
-    SELECT content
-    FROM clipboard
-    WHERE id = 1
-  `).get();
-  res.json({ content: row.content });
+  try {
+    const row = db.query(`
+      SELECT content
+      FROM clipboard
+      WHERE id = 1
+    `).get();
+    res.json({ content: row.content });
+  } catch (err) {
+    res.status(500).json({ error: 'Error while reading' });
+  }
 });
 
 router.post('/clipboard', (req, res) => {
   const { content } = req.body;
-  db.query(`
-    UPDATE clipboard
-    SET content = ?,
-      updated_at = CURRENT_TIMESTAMP
-    WHERE id = 1
-  `).run(content);
-  res.json({ message: 'Copied to clipboard' });
+  if (content === undefined) {
+    return res.status(400).json({ error: 'Field content is required' });
+  }
+  try {
+    db.query(`
+      UPDATE clipboard
+      SET content = ?,
+        updated_at = CURRENT_TIMESTAMP
+      WHERE id = 1
+    `).run(content);
+    res.json({ message: 'Copied to clipboard' });
+  } catch (err) {
+    res.status(500).json({ error: 'Error while writing' });
+  }
 });
 
 
